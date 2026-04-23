274acfe3f093dce7248cb169c3e948391eada674
Samarquesan <sam.samarques28@gmail.com>
2026-04-23 15:14:36 -0300

    atualizações

---
+++ js/env.js
@@ -0,0 +1,4 @@
+const ENV = {
+  SUPABASE_URL: "https://cjmelwvjpgdtgxtucmeq.supabase.co",
+  SUPABASE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNqbWVsd3ZqcGdkdGd4dHVjbWVxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYxMTIwOTAsImV4cCI6MjA5MTY4ODA5MH0.X_lNchks3ZShmbeLfAliNFJNw8-Ns65xkO9V0UjOSZg"
+};
